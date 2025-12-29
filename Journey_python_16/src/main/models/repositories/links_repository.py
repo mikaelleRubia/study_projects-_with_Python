9f5211b66e5056beb59f00687eff0b5f70cbd700
05792803579 <mrubia@apoioprodesp.sp.gov.br>
2025-12-29 11:50:32 -0300

    info:  adding find_trip_id_links

--- Journey_python_16/src/main/models/repositories/links_repository.py
+++ Journey_python_16/src/main/models/repositories/links_repository.py
@@ -1,9 +1,11 @@
 from sqlite3 import Connection
-from typing import Dict, Tuple
+from typing import Dict, List, Tuple
 
 class LinkRepository:
-    def __init__(self, conn: Connection )-> None:
-        self.__conn = conn
+    def __init__(self, conn: Connection) -> None:
+            if conn is None:
+                raise Exception("Conexão com o banco de dados não pode ser None")
+            self.__conn = conn
 
 
     def create_links(self, links: Dict):
@@ -31,6 +33,26 @@ class LinkRepository:
         )
         link = cursor.fetchone()
         return link
+    
+    def find_trip_id_links(self, trip_id: str)-> Tuple:
+        cursor = self.__conn.cursor()
+        cursor.execute(
+            '''
+            SELECT * FROM 'links' WHERE trip_id =? 
+            ''', (trip_id, )
+        )
+        links = cursor.fetchall()
+        return links
+    
+    def find_links_all(self)-> List:
+        cursor = self.__conn.cursor()
+        cursor.execute(
+            '''
+            SELECT * FROM 'links' 
+            '''
+        )
+        links = cursor.fetchall()
+        return links
     
     def update_links_link(self, link_id: str):
         cursor = self.__conn.cursor()

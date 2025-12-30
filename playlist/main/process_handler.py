9e4d5a8dd69afd817443551b3107be9cb61a7c17
05792803579 <mrubia@apoioprodesp.sp.gov.br>
2025-12-29 21:24:21 -0300

    feat: adding start

---
+++ playlist/main/process_handler.py
@@ -0,0 +1,15 @@
+from view.first_view import introduction_page
+from .constructor.song_register_constructor import song_register_process
+
+def start() -> None:
+    while True:
+        command = introduction_page()
+
+        if command == "1":
+            song_register_process()
+        elif command == "2":
+            print("Criando Playlist")
+        elif command == "5":
+            exit()
+        else:
+            print("\n comando nao encontrado, tente novamente! \n\n")

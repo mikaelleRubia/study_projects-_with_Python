9f4464354851fb1d1ea30dd3ba5c1828be8b4ebf
05792803579 <mrubia@apoioprodesp.sp.gov.br>
2025-12-29 21:24:04 -0300

    feat: adding constructor

---
+++ playlist/main/constructor/song_register_constructor.py
@@ -0,0 +1,6 @@
+from view.song_register_view import SongRegisterView
+
+
+def song_register_process():
+    song_register_view = SongRegisterView()
+    new_song = song_register_view.registry_song_initial()

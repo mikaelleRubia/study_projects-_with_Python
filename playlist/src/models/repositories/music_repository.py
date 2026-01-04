f3243c55aa17e68086dc24929878dd4205e03f52
05792803579 <mrubia@apoioprodesp.sp.gov.br>
2026-01-04 17:19:59 -0300

    Implementing music repository

---
+++ playlist/src/models/repositories/music_repository.py
@@ -0,0 +1,21 @@
+from src.models.entities.music import Music
+
+
+class __MusicRepository:
+    def __init__(self):
+        self.__music_list = []
+
+    
+    def insert_music(self, music: Music):
+        self.__music_list.append(music)
+
+    def find_music(self, music_title: str) -> Music:
+        for music in self.__music_list:
+            if music.title == music_title:
+                return music
+            
+    def get_all_songs(self) ->list:
+        return self.__music_list
+
+# SIngleton - Design Pattern
+musics_repository = __MusicRepository()

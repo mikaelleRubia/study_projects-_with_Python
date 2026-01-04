d327dca344cc4bb05eaa51d4a75fbf208cdb22df
05792803579 <mrubia@apoioprodesp.sp.gov.br>
2026-01-04 15:37:29 -0300

    feat: adding logic to song_register_process in the constructor

--- playlist/src/main/constructor/song_register_constructor.py
+++ playlist/src/main/constructor/song_register_constructor.py
@@ -1,6 +1,16 @@
 from ...view.song_register_view import SongRegisterView
-
+from src.controller.song_register_controller import SongRegisterController
 
 def song_register_process():
     song_register_view = SongRegisterView()
+    song_register_controller = SongRegisterController()
+
     new_song = song_register_view.registry_song_initial()
+    response = song_register_controller.insert(new_song)
+
+    if response["success"]:
+        song_register_view.register_song_success(response)
+    else:
+        song_register_view.register_song_fail(response)
+
+    

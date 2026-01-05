87f9f21996d9429c55ec85e7a7d646806d52dc7b
05792803579 <mrubia@apoioprodesp.sp.gov.br>
2026-01-05 11:01:53 -0300

    feat: adding playlist_creator_process in the process_handler

--- playlist/src/main/process_handler.py
+++ playlist/src/main/process_handler.py
@@ -1,3 +1,4 @@
+from src.main.constructor.playlist_creator_constructor import playlist_creator_process
 from ..view.first_view import introduction_page
 from .constructor.song_register_constructor import song_register_process
 
@@ -8,7 +9,7 @@ def start() -> None:
         if command == "1":
             song_register_process()
         elif command == "2":
-            print("Criando Playlist")
+            playlist_creator_process()
         elif command == "5":
             exit()
         else:

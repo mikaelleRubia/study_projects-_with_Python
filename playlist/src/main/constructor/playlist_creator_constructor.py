9824cbc522226bd1ec9fc6654e1b1264e1d1df9b
05792803579 <mrubia@apoioprodesp.sp.gov.br>
2026-01-05 11:00:56 -0300

    feat: adding playlist_creator_process

---
+++ playlist/src/main/constructor/playlist_creator_constructor.py
@@ -0,0 +1,13 @@
+from ...view.playlist_creator_view import PlaylistCreatorView
+from src.controller.playlist_creator_controller import PlaylistCreatorController
+
+def playlist_creator_process():
+    playlist_creator_view = PlaylistCreatorView()
+    playlist_creator_controller = PlaylistCreatorController()
+
+    response = playlist_creator_controller.create_playlist()
+    if response["success"]:
+       playlist_creator_view.playlist_creator_sucess(response)
+    else:
+        playlist_creator_view.register_song_fail(response)
+    

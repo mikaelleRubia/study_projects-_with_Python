6dfaffb532aab6c8b93a007d3f1bc93840bc2215
05792803579 <mrubia@apoioprodesp.sp.gov.br>
2026-01-12 19:45:42 -0300

    feat: class User

--- sample-flask-auth/models/user.py
+++ sample-flask-auth/models/user.py
@@ -1,6 +1,9 @@
-from app import db
+from extensions.database import db
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(db.Model, UserMixin):
+    __tablename__ = "users"
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
-    password = db.Column(db.String(80),nullable=False)
+    password = db.Column(db.String(80), nullable=False)

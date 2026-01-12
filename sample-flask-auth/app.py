462dcda16744312527501831c2a008099bf9cdfd
05792803579 <mrubia@apoioprodesp.sp.gov.br>
2026-01-12 19:45:03 -0300

    feat: adding route login

--- sample-flask-auth/app.py
+++ sample-flask-auth/app.py
@@ -1,15 +1,38 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify, request
+from extensions.database import db
+from extensions.login import login_manager
+from flask_login import login_user, current_user
+from models.user import User
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///database.db'
-db = SQLAlchemy(app)
 
 
-@app.route("/hello", methods=["GET"])
-def hello():
-    return "oi"
+db.init_app(app)
+login_manager.init_app(app)
+
+login_manager.login_view= 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            login_user(user)
+            print(current_user.is_authenticated)
+            return jsonify({"message":"Autenticação realizada com sucesso"}), 200
+    return jsonify({"message":"Credenciais inválidas"}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)

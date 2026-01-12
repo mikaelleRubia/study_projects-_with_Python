07691c96fd1f3e86084bd6bd188f8aea030156a9
05792803579 <mrubia@apoioprodesp.sp.gov.br>
2026-01-12 14:04:34 -0300

    feat: adding test_get_tasks

--- Introdutorio/tests.py
+++ Introdutorio/tests.py
@@ -2,7 +2,7 @@ import pytest
 import requests
 
 BASE_URL = 'http://127.0.0.1:5000'
-task = []
+tasks = []
 
 def test_create_task():
     new_task = {
@@ -10,5 +10,18 @@ def test_create_task():
     "description":"descrição de teste"
     }
     reponse = requests.post(f"{BASE_URL}/tasks", json=new_task)
+    result = reponse.json()
 
     assert reponse.status_code ==201
+    assert 'id' in result["task"]
+    tasks.append(result["task"]['id'])
+
+
+def test_get_tasks():
+    reponse = requests.get(f"{BASE_URL}/tasks")
+    result = reponse.json()
+
+    assert reponse.status_code ==200
+    assert 'Tasks' in result
+    assert isinstance(result['Tasks'], list)
+
